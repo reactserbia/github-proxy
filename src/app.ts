ef630a6d7db6db94dcbc25a263df498e5526b83b
Mirko Basic <bejzik8@gmail.com>
2023-10-27 18:55:35 +0200

    Added unmatched route handler.

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,8 @@ app.use(json())
 
 app.use('/github', githubRoutes)
 
+app.use((_, res) => res.status(404).json({ message: 'Invalid Request' }))
+
 app.use((err: Error, _: Request, res: Response, _2: NextFunction) => {
     res.status(500).json({ message: err.message })
 })
