9df481aac09e76b5c961ae7fc4b1e3f32bb7183b
Mirko Basic <bejzik8@gmail.com>
2023-10-27 18:35:53 +0200

    Environment variables added.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { json, Request, Response, NextFunction } from 'express'
+import 'dotenv/config'
 
 import githubRoutes from './routes/github'
 
--- src/controllers/github.ts
+++ src/controllers/github.ts
@@ -1,5 +1,8 @@
 import { RequestHandler } from 'express'
 
 export const getGitHubAccessToken: RequestHandler = (req, res, next) => {
+    const body = req.body
+    console.log(body)
+
     res.status(200).json({ message: 'Hello from GitHub' })
 }
