f2bccee87b01d0066030ffca9b255858bed849d2
Mirko Basic <bejzik8@gmail.com>
2023-10-27 18:12:21 +0200

    Added error handling midleware.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 
 import githubRoutes from './routes/github'
 
@@ -7,5 +7,9 @@ const app = express()
 app.use(express.json())
 
 app.use('/github', githubRoutes)
+
+app.use((err: Error, _: Request, res: Response, _2: NextFunction) => {
+    res.status(500).json({ message: err.message })
+})
 
 app.listen(3000, () => console.log('Server is running'))
