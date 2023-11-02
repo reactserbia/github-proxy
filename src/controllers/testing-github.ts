b35fd9ff51c5bfecfaddfbc7400e9086d8c6e3c9
Mirko Basic <bejzik8@gmail.com>
2023-11-02 20:36:53 +0100

    Added getUserByUsername function.

--- src/controllers/testing-github.ts
+++ src/controllers/testing-github.ts
@@ -2,13 +2,30 @@ import { RequestHandler } from 'express'
 
 import { octokit } from '../api'
 
-export const getGitHubUserProfile: RequestHandler = (_, res, next) => {
+export const getLoggedInUser: RequestHandler = (_, res, next) => {
     octokit
         .request('GET /user')
         .then(response => {
-            res.status(200).json({ data: response })
+            res.status(200).json({ data: response.data })
         })
         .catch(err => {
+            next(err)
+        })
+}
+
+export const getUserByUsername: RequestHandler = (req, res, next) => {
+    const { username } = req.params
+
+    console.log(username)
+
+    octokit
+        .request(`GET /users/${username}`)
+        .then(response => {
+            console.log(response)
+            res.status(200).json({ data: response.data })
+        })
+        .catch(err => {
+            console.log(err)
             next(err)
         })
 }
