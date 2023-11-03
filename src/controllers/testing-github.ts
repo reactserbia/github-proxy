5cc6e3e675ca41fba38be943932353d17ebd29d5
Mirko Basic <bejzik8@gmail.com>
2023-11-03 01:13:15 +0100

    Working on delete all contents functionality...

--- src/controllers/testing-github.ts
+++ src/controllers/testing-github.ts
@@ -1,4 +1,7 @@
 import { RequestHandler } from 'express'
+import { simpleGit } from 'simple-git'
+import * as fs from 'fs'
+import * as path from 'path'
 
 import { octokit } from '../api'
 
@@ -52,36 +55,66 @@ export const forkRepository: RequestHandler = (req, res, next) => {
         })
 }
 
-export const deleteFilesInRepository: RequestHandler = (req, res, next) => {
-    const { owner, repo } = req.params
+// export const deleteFilesInRepository: RequestHandler = (req, res, next) => {
+//     const { owner, repo } = req.params
 
-    const deleteFiles: any = (path = '') =>
-        octokit.repos
-            .getContent({ owner, repo, path })
-            .then(({ data }) => {
-                if (Array.isArray(data)) {
-                    return Promise.all(
-                        data.map(file => {
-                            if (file.type === 'dir') {
-                                return deleteFiles(file.path)
-                            } else {
-                                return octokit.repos.deleteFile({
-                                    owner,
-                                    repo,
-                                    path: file.path,
-                                    message: 'delete file',
-                                    sha: file.sha
-                                })
-                            }
-                        })
-                    )
+//     const deleteFiles: any = (path = '') =>
+//         octokit.repos
+//             .getContent({ owner, repo, path })
+//             .then(({ data }) => {
+//                 if (Array.isArray(data)) {
+//                     return Promise.all(
+//                         data.map(file => {
+//                             if (file.type === 'dir') {
+//                                 return deleteFiles(file.path)
+//                             } else {
+//                                 return octokit.repos.deleteFile({
+//                                     owner,
+//                                     repo,
+//                                     path: file.path,
+//                                     message: 'delete file',
+//                                     sha: file.sha
+//                                 })
+//                             }
+//                         })
+//                     )
+//                 }
+//             })
+//             .catch(err => console.error(err))
+
+//     deleteFiles()
+//         .then(() => {
+//             res.status(200).json({ data: 'Data deleted.' })
+//         })
+//         .catch((err: Error) => {
+//             next(err)
+//         })
+// }
+
+const repoUrl = 'https://github.com/bejzik8/itmilos.git'
+const localPath = path.resolve(__dirname, 'repo')
+
+const git = simpleGit()
+
+export const deleteFilesInRepository: RequestHandler = (req, res, next) => {
+    git.clone(repoUrl, localPath)
+        .then(() => git.cwd(localPath))
+        .then(() =>
+            fs.readdirSync(localPath).forEach(file => {
+                if (file !== '.git') {
+                    fs.rmSync(path.join(localPath, file), {
+                        recursive: true,
+                        force: true
+                    })
                 }
             })
-            .catch(err => console.error(err))
-
-    deleteFiles()
+        )
+        .then(() => git.add('./*'))
+        .then(() => git.commit('Deleted all files and folders.'))
+        .then(() => git.push('origin', 'main'))
         .then(() => {
-            res.status(200).json({ data: 'Data deleted.' })
+            fs.rmdirSync(localPath, { recursive: true })
+            return res.status(200).json({ data: 'Data deleted.' })
         })
         .catch((err: Error) => {
             next(err)
